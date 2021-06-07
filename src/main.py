33653caa898ff22e31c7eeb08fa10205cb45e936
Indominus22 <82047079+Indominus22@users.noreply.github.com>
2021-06-07 18:06:16 -0400

    prototipo de combate

--- src/main.py
+++ src/main.py
@@ -1,5 +1,33 @@
 from datos_pokemo import informacion as info_poke
+from datos_pokemo import estadisticas
+turno=0
+
 
 primer_pokemo = info_poke.DatosBasico("Pikachu", 125, 40)
 segundo_pokemom = info_poke.DatosBasico("Chakka", 200, 35)
 
+print(primer_pokemo.hp)
+primer_pokemo.set_dagno_recibido(35)
+print(primer_pokemo.get_hp())
+primer_pokemo.set_dagno_recibido(35)
+print(primer_pokemo.get_hp())
+#demo de combate
+while(primer_pokemo.get_hp()>0 and segundo_pokemom.get_hp()>0):
+    if(turno==0):
+        #turno de Chkka
+        primer_pokemo.set_dagno_recibido(35)
+        print("pikachu:",primer_pokemo.get_hp())
+        if(primer_pokemo.get_hp()<=0):
+            print("Pikachu ha sido debilitado")
+            break
+        else:
+            turno=turno+1
+    if(turno==1):
+        #turno de pikachu
+        segundo_pokemom.set_dagno_recibido(40)
+        print("Chakka:",segundo_pokemom.get_hp())
+        if(segundo_pokemom.get_hp()<=0):
+            print("Chakka ha sido eliminado")
+            break
+        else:
+            turno=turno-1
