88e124b453ecec4fc7f3eaf4de7d4f72c60fdd15
Coragg <v_e_c_g@outlook.com>
2021-06-07 18:57:45 -0400

    ataques y limpieza de codigo

--- src/main.py
+++ src/main.py
@@ -1,16 +1,10 @@
 from datos_pokemo import informacion as info_poke
 from datos_pokemo import estadisticas
-turno=0
+turno = 0
 
 
-primer_pokemo = info_poke.DatosBasico("Pikachu", 125, 100)
+primer_pokemo = info_poke.DatosBasico("Pikachu", 125, 45)
 segundo_pokemom = info_poke.DatosBasico("Chakka", 200, 35)
-
-print(primer_pokemo.hp)
-primer_pokemo.set_dagno_recibido(35)
-print(primer_pokemo.get_hp())
-primer_pokemo.set_dagno_recibido(35)
-print(primer_pokemo.get_hp())
 #demo de combate
 while primer_pokemo.get_hp() > 0 and segundo_pokemom.get_hp() > 0:
     if turno == 0:
@@ -23,7 +17,7 @@ while primer_pokemo.get_hp() > 0 and segundo_pokemom.get_hp() > 0:
             break
         else:
             turno += 1
-    if(turno==1):
+    if turno == 1:
         #turno de pikachu
         ataque = primer_pokemo.damages
         segundo_pokemom.set_dagno_recibido(ataque)
@@ -32,4 +26,4 @@ while primer_pokemo.get_hp() > 0 and segundo_pokemom.get_hp() > 0:
             print("Chakka ha sido debilitado")
             break
         else:
-            turno=turno-1
+            turno-= 1
