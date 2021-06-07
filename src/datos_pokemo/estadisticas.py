4c344b34af05f9d4c40298a813d1aff0150c1d1f
sccaceres <s.castrocaceres@uandresbello.edu>
2021-06-07 19:47:12 -0400

    def modificar

--- src/datos_pokemo/estadisticas.py
+++ src/datos_pokemo/estadisticas.py
@@ -1,2 +1,12 @@
+import random
+
 def suma(sumando1: eval, sumando2: eval):
     return sumando1 + sumando2
+
+def modificar(Type,STAB):
+    rando= random.uniform(0.85,1)
+    mofifier= Type * STAB * rando * 1
+    return round(mofifier,2)
+
+hola= modificar(5,2)
+print(hola)
