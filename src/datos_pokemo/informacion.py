20c0e97108c30dcf0c9c72dd81a0ab2fc66f6525
Coragg <v_e_c_g@outlook.com>
2021-06-07 16:57:09 -0400

    uso de clases

---
+++ src/datos_pokemo/informacion.py
@@ -0,0 +1,20 @@
+class DatosBasico:
+    name_pokemon: str
+    tipos: list
+    hp: int
+    ataque: int
+    level: int
+    velocidad: int
+
+    def set_hp(self, vida):
+        self.hp = vida
+
+    def get_hp(self, vida):
+        return vida
+
+
+
+
+
+
+lista = []
--- src/main.py
+++ src/main.py
@@ -1,2 +1,7 @@
+from datos_pokemo import informacion as info
+
+pokemon = info.DatosBasico()
+
+
 print("wenas")
 print("como estan")
