3f7a8aca1d832bf48b446c9aa958dd542ec0fbab
sccaceres <s.castrocaceres@uandresbello.edu>
2021-06-07 20:07:11 -0400

    def hp

--- src/datos_pokemo/estadisticas.py
+++ src/datos_pokemo/estadisticas.py
@@ -1,12 +1,13 @@
 import random
+import math
 
-def suma(sumando1: eval, sumando2: eval):
-    return sumando1 + sumando2
+def HP(Base):
+    Level, IV, EV = 50, 31, 250
+    hp=(((((Base+IV)*2)+((math.sqrt(EV))/4))*Level)/100) + Level + 10
+    return hp
 
 def modificar(Type,STAB):
     rando= random.uniform(0.85,1)
     mofifier= Type * STAB * rando * 1
     return round(mofifier,2)
 
-hola= modificar(5,2)
-print(hola)
