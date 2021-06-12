a0c40afd16f8f808c73504883e184cd02a64ff53
Coragg <v_e_c_g@outlook.com>
2021-06-12 16:03:09 -0400

    bases para recorrer la información del pokemon

--- src/lista_pokemon/read_document.py
+++ src/lista_pokemon/read_document.py
@@ -1,4 +1,20 @@
 read_file = open("pokemon_data.csv", "r")
-read = read_file.read()
-print(read)
+
+
+# datos separados por salto de linea
+informacion_salto_linea = []
+for leer_linea_por_linea in read_file:
+    listar_data = leer_linea_por_linea.replace('\n', "").split(sep=",")
+    informacion_salto_linea.append(listar_data)
+
+# print(informacion_salto_linea[0])
+name_attack = ""
+
+print(type(informacion_salto_linea[0][8]))
+cortar = informacion_salto_linea[0][8].split(sep=";")
+print(cortar)
+for recorer in cortar:
+    print(recorer)
+
+
 read_file.close()
